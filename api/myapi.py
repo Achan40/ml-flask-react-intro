011c6c7db1df89f033e0ea4dd548f6b9d3231da6
Aaron <chanman2841@gmail.com>
2021-08-21 22:58:57 -0500

    basic post to return a predicted value from ML model complete

--- api/myapi.py
+++ api/myapi.py
@@ -1,10 +1,15 @@
-from logging import debug
-import re
+import pickle
+import pandas as pd
+
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+# use pickle to load in the trained model
+with open(f'model/iris_rfclass.pkl','rb') as f:
+    model = pickle.load(f)
 
 # Flask automatically returns a json response if returned message is a python dict
 class MyFirst(Resource):
@@ -16,8 +21,12 @@ class MyFirst(Resource):
     
     def post(self):
         data = request.get_json(force=True)
-        
-        return data
+
+        # Requires keys SepalL,SepalW,PetalL,PetalW (parameters for model) in order to return the output of the model
+        input_variables = pd.DataFrame([[data['SepalL'], data['SepalW'], data['PetalL'], data['PetalW']]], columns=['Sepal.Length','Sepal.Width','Petal.Length','Petal.Width'], dtype=float)
+        prediction = model.predict(input_variables)[0]
+
+        return prediction
 
 api.add_resource(MyFirst, '/')
 
