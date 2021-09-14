a97fc688f2ebe27953b3b129c23efd258edda757
Aaron <chanman2841@gmail.com>
2021-09-14 16:16:28 -0500

    complete display of text for various clients using websocket

--- frontend/front/src/components/AppWebSocket.js
+++ frontend/front/src/components/AppWebSocket.js
@@ -6,6 +6,9 @@ const client = new W3CWebSocket('ws://127.0.0.1:8000');
 class AppWebSocket extends Component {
     constructor() {
         super()
+        this.state = {
+            messages: []
+        }
         this.onButtonClicked = this.onButtonClicked.bind(this)
     }
 
@@ -23,6 +26,15 @@ class AppWebSocket extends Component {
         client.onmessage = (message) => {
             const dataFromServer = JSON.parse(message.data);
             console.log('Received reply: ', dataFromServer);
+            if (dataFromServer.type === "message") {
+                this.setState((state) =>
+                ({
+                    messages: [...state.messages,
+                    {
+                        msg: dataFromServer.msg
+                    }]
+                }))
+            }
         };
     }
 
@@ -31,6 +43,7 @@ class AppWebSocket extends Component {
             <div className="main">
                 <p>Websocket Component</p>
                 <button onClick={() => this.onButtonClicked("Hello World!")}>Send a message to websocket server</button>
+                {this.state.messages.map(msg => <p>message: {msg.msg}</p>)}
             </div>
         )
     }
