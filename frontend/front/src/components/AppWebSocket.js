90b538b780dfde1a22e141735b29daf06ed6ed1e
Aaron <chanman2841@gmail.com>
2021-09-14 10:45:19 -0500

    send message to websocket server and console log response

--- frontend/front/src/components/AppWebSocket.js
+++ frontend/front/src/components/AppWebSocket.js
@@ -4,20 +4,33 @@ import { w3cwebsocket as W3CWebSocket } from "websocket";
 const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
 class AppWebSocket extends Component {
+    constructor() {
+        super()
+        this.onButtonClicked = this.onButtonClicked.bind(this)
+    }
+
+    onButtonClicked = (value) => {
+        client.send(JSON.stringify({
+            type: "message",
+            msg: value
+        }));
+    }
 
     componentDidMount() {
         client.onopen = () => {
             console.log('WebSocket Client Connected');
         };
         client.onmessage = (message) => {
-            console.log(message);
+            const dataFromServer = JSON.parse(message.data);
+            console.log('Received reply: ', dataFromServer);
         };
     }
 
     render() {
         return (
-            <div>
-                <p>Websocket component</p>
+            <div className="main">
+                <p>Websocket Component</p>
+                <button onClick={() => this.onButtonClicked("Hello World!")}>Send a message to websocket server</button>
             </div>
         )
     }
