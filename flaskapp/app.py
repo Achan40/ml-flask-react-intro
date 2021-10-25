8bdfada6ca02bd986632b2bc3b9583ab8de3cb59
Aaron <chanman2841@gmail.com>
2021-10-24 19:33:48 -0500

    dockerize api, client, and websocket applications. Also add docker compose file to link them together

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -8,18 +8,19 @@ import pickle
 import pandas as pd
 
 # create flask object
-app = Flask(__name__, static_url_path='', static_folder='frontend/front')
+app = Flask(__name__)
 CORS(app) # comment out on deployment
 api = Api(app)
 
 # Edit the app.js file in react project directory for the frontend
-@app.route('/', defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+# @app.route('/', defaults={'path':''})
+# def serve(path):
+#     return send_from_directory(app.static_folder,'index.html')
 
-# add the API class from teh myapi.py file
+# add the API class from the myapi.py file
 api.add_resource(MyFirst, '/')
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
     # Set debug false when in production
     app.run(debug=True)
